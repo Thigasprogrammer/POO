81827d3ea08017e03e377f4dac2c679176a2d7b4
Thigasprogrammer <thiagoaguiar035@gmail.com>
2025-03-25 09:51:47 -0300

    Triangulo.java
    programa que recebe 3 valores de lado e verifica se é um triângulo, se for, se esse triângulo retângulo.

--- Triangulo.java
+++ Triangulo.java
@@ -7,9 +7,9 @@ public class Triangulo {
         
         String classific = ("É TRIÂNGULO, PORÉM, NÃO É TRIÂNGULO RETÂNGULO");
 
-		if((Math.pow(a, 2) + Math.pow(b, 2)) == Math.pow(c, 2)){
-            String classific1 = ("É TRIÂNGULO RETÂNGULO");
-            return classific1;
+	if((Math.pow(a, 2) + Math.pow(b, 2)) == Math.pow(c, 2)){
+	        String classific1 = ("É TRIÂNGULO RETÂNGULO");
+	        return classific1;
         }
         return classific;
 
@@ -17,24 +17,23 @@ public class Triangulo {
 	public static void main(String[] args){ 
 		Scanner digitar = new Scanner(System.in);
 		float A;
-        float B;
-        float C;
+	        float B;
+	        float C;
 		
 		System.out.print("DIGITE O PRIMEIRO LADO DO TRIÂNGULO, CATETO OPOSTO: "); 
 		A = digitar.nextInt();
-        System.out.print("DIGITE O SEGUNDO LADO DO TRIÂNGULO, CATETO ADJACENTE: "); 
+        	System.out.print("DIGITE O SEGUNDO LADO DO TRIÂNGULO, CATETO ADJACENTE: "); 
 		B = digitar.nextInt();
-        System.out.print("DIGITE O TERCEIRO LADO DO TRIÂNGULO, HIPOTENUSA: "); 
+        	System.out.print("DIGITE O TERCEIRO LADO DO TRIÂNGULO, HIPOTENUSA: "); 
 		C = digitar.nextInt(); 
 
 		if(A+B>=C && B+C>=A && A+C>=B){
         		System.out.print(classificacao(A, B, C));
+        	}
 
-        }
-
-        else{
-        System.out.print("NÃO É UM TRIÂNGULO");
-        }
+        	else{
+        		System.out.print("NÃO É UM TRIÂNGULO");
+        	}
 		
 	}
 
