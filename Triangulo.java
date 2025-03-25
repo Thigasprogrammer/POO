ceb3c97764f79050d52646da58f5da180d7e4cfc
Thigasprogrammer <thiagoaguiar035@gmail.com>
2025-03-25 16:18:06 -0300

    Triangulo.java

--- Triangulo.java
+++ Triangulo.java
@@ -7,27 +7,27 @@ public class Triangulo {
         
         String classific = ("É TRIÂNGULO, PORÉM, NÃO É TRIÂNGULO RETÂNGULO");
 
-	if((Math.pow(a, 2) + Math.pow(b, 2)) == Math.pow(c, 2)){
-	        String classific1 = ("É TRIÂNGULO RETÂNGULO");
+	if((Math.pow(a, 2) + Math.pow(b, 2)) == Math.pow(c, 2) || (Math.pow(b, 2) + Math.pow(c, 2)) == Math.pow(a, 2) || (Math.pow(c, 2) + Math.pow(a, 2)) == Math.pow(b, 2)){ // verifica as tres possibilidades do teorema de pitagoras
+		String classific1 = ("É TRIÂNGULO RETÂNGULO");
 	        return classific1;
         }
         return classific;
 
 	}
 	public static void main(String[] args){ 
-		Scanner digitar = new Scanner(System.in);
-		float A;
-	        float B;
-	        float C;
+    	Scanner digitar = new Scanner(System.in);
+    		float A;
+	    	float B;
+	    	float C;
 		
-		System.out.print("DIGITE O PRIMEIRO LADO DO TRIÂNGULO, CATETO OPOSTO: "); 
+		System.out.print("DIGITE O PRIMEIRO LADO DO TRIÂNGULO: "); 
 		A = digitar.nextInt();
-        	System.out.print("DIGITE O SEGUNDO LADO DO TRIÂNGULO, CATETO ADJACENTE: "); 
+       	 	System.out.print("DIGITE O SEGUNDO LADO DO TRIÂNGULO: "); 
 		B = digitar.nextInt();
-        	System.out.print("DIGITE O TERCEIRO LADO DO TRIÂNGULO, HIPOTENUSA: "); 
+        	System.out.print("DIGITE O TERCEIRO LADO DO TRIÂNGULO: "); 
 		C = digitar.nextInt(); 
 
-		if(A+B>=C && B+C>=A && A+C>=B){
+		if(A+B>C && B+C>A && A+C>B){
         		System.out.print(classificacao(A, B, C));
         	}
 
@@ -38,4 +38,3 @@ public class Triangulo {
 	}
 
 }
-
