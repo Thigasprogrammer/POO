a4a1c0258c5380edbdcb73b877b5d8ba45a0bcb8
Thigasprogrammer <thiagoaguiar035@gmail.com>
2025-04-02 20:29:25 -0300

    Tarefa4String.java

--- Tarefa4String.java
+++ Tarefa4String.java
@@ -3,23 +3,42 @@ import java.util.Scanner;
 public class Tarefa4String {
     public static void gerarLosango(int largura) {
         int metade = largura / 2;
-        String alfabeto = "ABCDEFGHIJKLMNO0PQRSTUVWXYZ";
-
-        // parte superior do losango
-        for(int i = 0; i <= metade; i++){
-            for (int j = 0; j < metade - i; j++) {
-                System.out.print(" ");
+        String alfabeto = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        if(largura % 2 == 0){ //verificr se a largura é par
+            // parte superior do losango par
+            for(int i = 0; i <= metade; i++){
+                for (int j = 0; j < metade - i; j++) {
+                    System.out.print(" ");
+                }
+                System.out.println(alfabeto.substring(0, 2 * i));
             }
-            System.out.println(alfabeto.substring(0, 2 * i + 1));
-        }
 
-        // parte inferior do losango
-        for(int i = metade - 1; i >= 0; i--){
-            for (int j = 0; j < metade - i; j++) {
-                System.out.print(" ");
+            // parte inferior do losango par
+            for(int i = metade - 1; i >= 0; i--){
+                for (int j = 0; j < metade - i; j++) {
+                    System.out.print(" ");
+                }
+                System.out.println(alfabeto.substring(0, 2 * i));
             }
-            System.out.println(alfabeto.substring(0, 2 * i + 1));
         }
+        else{ //verificr se a largura é ímpar
+            // parte superior do losango ímpar
+            for(int i = 0; i <= metade; i++){
+                for (int j = 0; j < metade - i; j++) {
+                    System.out.print(" ");
+                }
+                System.out.println(alfabeto.substring(0, 2 * i + 1));
+            }
+
+            // parte inferior do losango ímpar
+            for(int i = metade - 1; i >= 0; i--){
+                for (int j = 0; j < metade - i; j++) {
+                    System.out.print(" ");
+                }
+                System.out.println(alfabeto.substring(0, 2 * i + 1));
+            }
+        }
+        
 
 
 
@@ -29,11 +48,6 @@ public class Tarefa4String {
         Scanner teclado = new Scanner(System.in);
         System.out.print("Digite a largura do losango: ");
         int largura = teclado.nextInt();        
-        while(largura % 2 == 0 ){
-            System.out.print("Não é possível gerar o losango com esse número, digite um impar e positivo: ");
-            largura = teclado.nextInt();        
-
-        }
         gerarLosango(largura);
         
     }
