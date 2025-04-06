041e702a69e3c752433a0f3e4a1a51b83d6b2df4
Thigasprogrammer <thiagoaguiar035@gmail.com>
2025-04-06 20:55:14 -0300

    Desafio2String.java

--- Desafio2String.java
+++ Desafio2String.java
@@ -1,50 +1,46 @@
-// tirar pontos e virgulas após terminar o codigo
-// teste.charAt(i).isDigit() (verifica se a caractere é um digito)
-// teste.charAt(i).isLetter() (verifica se o caractere é uma letra)
-
-
 import java.util.Scanner;
 public class Desafio2String {
-	public static void Foundwords(String txt){
-	    String word = "";
-		int letter;
-		String biggestword = "";
-		String smallestword = ""; 
+    public static void Foundwords(String txt){
+        String word = "";
+        String biggestword = "";
+        String smallestword = ""; 
         
         System.out.print("PALAVRAS ENCONTRADAS \n" + "====================\n");
-		for(int i = 0; i < txt.length(); i++){
+        for(int i = 0; i < txt.length(); i++){
+    
+        if((txt.charAt(i) != ' ') && (txt.charAt(i) != '.') && (txt.charAt(i) != ',') && (txt.charAt(i) != ':')){
+                word = word + txt.charAt(i);
+            }
+            else{
+                if(word.length() >= 4){
+                    if (word.length()>biggestword.length()){
+                    biggestword = word;
+                    }
+                    if (word.length() <= smallestword.length() || smallestword == ""){
+                        smallestword = word;
+                    }
+                    System.out.println(word);
+                    word = "";
+                } 
+                else{
+                    word = "";
+                }
+            }
+        
+        }
+        if(word.length() >= 4){
+            System.out.println(word);
+        }
+        System.out.println("MAIOR PALAVRA ENCONTRADA: " + biggestword);
+        System.out.println("MENOR PALAVRA ENCONTRADA: " + smallestword);
 
-			if(txt.charAt(i) != ' '){
-				word = word + txt.charAt(i);
-			}
-			else{
-				if(word.length() >= 4){
-					if (word.length()>biggestword.length()){
-			    	biggestword = word;
-				    }
-				    if (word.length() <= smallestword.length() || smallestword == ""){
-				    	smallestword = word;
-				    }
-					System.out.println(word);
-					word = "";
-				} 
-				else{
-					word = "";
-				}
-			}
-		
-		}
-		if(word.length() >= 4){
-		    System.out.println(word);
-		}
-		System.out.println("MAIOR PALAVRA ENCONTRADA: " + biggestword);
-		System.out.println("MENOR PALAVRA ENCONTRADA: " + smallestword);
 
-
-	} //Foundwords
+    } //Foundwords
     public static void main(String[] args){
-        String a = ": Escreva um programa em Java que percorra um texto tipo String e exiba na tela as palavras que forem sendo encontradas. Descarte palavras com menos de 4 letras. Ao final, o programa deve exibir a maior e a menor palavra que foi exibida na tela. O texto de entrada é fornecido pelo usuário. ";
-    	Foundwords(a);
+        Scanner write = new Scanner(System.in);
+        System.out.println("Digite abaixo o texto a ser analisado: \n");
+        String text = write.nextLine();
+        Foundwords(text);
 
     }//main
 }//class
